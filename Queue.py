3a3c13b6e9f593685afca1433ced0b4814c1c013
Taylor J Saintable <tsaintable@gmail.com>
2020-03-25 17:12:53 -0200

    Made stack & queue a class

--- Queue.py
+++ Queue.py
@@ -1,24 +1,37 @@
 # Last in first out (LIFO)
-queue = [] 
 
-queue.append('John') 
-queue.append('Brian') 
-queue.append('Freddie') 
-queue.append('Emma')
-queue.append('Alison') 
-queue.append('Jessica') 
+class Queue:
+    def __init__(self):
+        self.queue = []
 
-print("Initial queue") 
-print(queue) 
+    def enqueue(self,val):
+        self.queue.insert(0,val)
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.queue.pop()
+
+    def size(self):
+        return len(self.queue)
+
+    def is_empty(self):
+        return self.size() == 0
+
+queue = Queue()
+
+queue.enqueue('John') 
+queue.enqueue('Brian') 
+queue.enqueue('Freddie') 
+queue.enqueue('Emma')
+queue.enqueue('Alison') 
+queue.enqueue('Jessica') 
 
 print("\nElements dequeued from queue") 
-print(queue.pop(0)) 
-print(queue.pop(0)) 
-print(queue.pop(0)) 
-print(queue.pop(0)) 
-print(queue.pop(0)) 
-print(queue.pop(0)) 
-
-print("\nQueue after removing elements") 
-print(queue) 
-
+print(queue.dequeue()) 
+print(queue.dequeue()) 
+print(queue.dequeue()) 
+print(queue.dequeue()) 
+print(queue.dequeue()) 
+print(queue.dequeue()) 
--- Stack.py
+++ Stack.py
@@ -1,15 +1,38 @@
 # First in First out (FIFO)
-stack = [] 
 
-stack.append('John') 
-stack.append('Brian') 
-stack.append('Freddie') 
-stack.append('Emma')
-stack.append('Alison') 
-stack.append('Jessica') 
+class Stack:
+    def __init__(self):
+        self.stack = []
 
-print('Initial stack') 
-print(stack) 
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack.pop()
+
+    def push(self,val):
+        return self.stack.append(val)
+
+    def peak(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
+
+    def is_empty(self):
+        return self.size() == 0
+
+stack = Stack()
+
+stack.push('John') 
+stack.push('Brian') 
+stack.push('Freddie') 
+stack.push('Emma')
+stack.push('Alison') 
+stack.push('Jessica') 
 
 print('\nElements poped from stack:') 
 print(stack.pop()) 
@@ -18,6 +41,3 @@ print(stack.pop())
 print(stack.pop()) 
 print(stack.pop()) 
 print(stack.pop()) 
-
-print('\nStack after elements are poped:') 
-print(stack)
